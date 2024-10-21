c61ab637c8c94e2cbb6fa778f9b4490803cc3463
pankajkrishnavanshaj <pankajyaduvanshi108@gmail.com>
2024-10-21 21:43:14 +0530

    auth

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -27,17 +27,6 @@ export const authOptions = {
             email,
             name,
             username: defaultUsername,
-            secondemail: "",
-            bio: "",
-            number: "",
-            twitter: "",
-            facebook: "",
-            instagram: "",
-            linkedin: "",
-            avatar: "",
-            github: "",
-            resume: "",
-            skill: [],
           });
           return true;
         } catch (error) {
@@ -54,17 +43,6 @@ export const authOptions = {
         token.email = dbUser.email;
         token.name = dbUser.name;
         token.username = dbUser.username;
-        token.secondemail = dbUser.secondemail;
-        token.bio = dbUser.bio;
-        token.number = dbUser.number;
-        token.twitter = dbUser.twitter;
-        token.facebook = dbUser.facebook;
-        token.instagram = dbUser.instagram;
-        token.linkedin = dbUser.linkedin;
-        token.avatar = dbUser.avatar;
-        token.github = dbUser.github;
-        token.resume = dbUser.resume;
-        token.skill = dbUser.skill;
       }
       return token;
     },
@@ -73,17 +51,7 @@ export const authOptions = {
       session.user.email = token.email;
       session.user.name = token.name;
       session.user.username = token.username;
-      session.user.secondemail = token.secondemail;
-      session.user.bio = token.bio;
-      session.user.number = token.number;
-      session.user.twitter = token.twitter;
-      session.user.facebook = token.facebook;
-      session.user.instagram = token.instagram;
-      session.user.linkedin = token.linkedin;
-      session.user.avatar = token.avatar;
-      session.user.github = token.github;
-      session.user.resume = token.resume;
-      session.user.skill = token.skill;
+
       return session;
     },
   },
